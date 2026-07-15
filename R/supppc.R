a37d896af04ed065e4057eb53430302b4d0fb284
munoztd0 <dtord@its.jnj.com>
2026-07-15 14:55:18 +0000

    create new suppc

---
+++ R/supppc.R
@@ -0,0 +1,23 @@
+#' @title supppc
+#'
+#' @description supppc modified from pharmaversesdtm
+#' @source data from pharmaversesdtm.
+#'
+#' @format A data frame with 4572 rows and 8 variables:
+#' \describe{
+#'  \item{STUDYID}{Study Identifier}
+#'  \item{RDOMAIN}{Related Domain Abbreviation}
+#'  \item{USUBJID}{Unique Subject Identifier}
+#'  \item{IDVAR}{Identifying Variable}
+#'  \item{IDVARVAL}{Identifying Variable Value}
+#'  \item{QNAM}{Qualifier Variable Name}
+#'  \item{QLABEL}{Qualifier Variable Label}
+#'  \item{QVAL}{Qualifier Value}
+#' }
+#' @seealso \code{\link{ae}} \code{\link{ds}} \code{\link{dv}} \code{\link{ho}} \code{\link{ie}} \code{\link{mh}} \code{\link{pr}} \code{\link{suppho}} \code{\link{supppc}} \code{\link{supppr}} \code{\link{ts}}
+#' @keywords datasets supppc
+#' @name supppc
+#' @examples
+#'  head(data("supppc"))
+"supppc"
+
